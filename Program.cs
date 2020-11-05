bec8d48d908056ccb6d7a0006383641408b83602
Marcelo11114 <dedosearte@hotmail.com>
2020-11-05 09:43:55 -0300

    Tabuada

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Tabuada
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string Sair;
+            do{
+            Console.Clear();
+            int Mult;
+           
+            Console.WriteLine("      +---------+");
+            Console.WriteLine("      | TABUADA |");
+            Console.WriteLine("      +---------+\n");
+            Console.Write("Qual tabuada deseja ?");
+            Mult = Convert.ToInt16(Console.ReadLine());
+
+            for (int i=0; i<=10; i++)
+            {
+                Console.WriteLine($"{Mult} X {i} = {Mult*i}");
+            }
+
+            Console.Write("n/Outra ?(s)");
+            Sair=Console.ReadLine().ToUpper();
+            } while (Sair == "S");
+            Console.Clear();
+
+
+
+
+
+
+            
+        }
+    }
+}
